11ed7bfc79104d50bc252e4df5351c457361650f
josecas <jmcg0901@gmail.com>
2022-11-04 19:42:39 -0600

    Folder setup

---
+++ TwitterSwiftUI/Core/Components/Tweets/TweetRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetRowView.swift
+//  TwitterSwiftUI
+//
+//  Created by Jose Castillo on 11/4/22.
+//
+
+import SwiftUI
+
+struct TweetRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetRowView()
+    }
+}
---
+++ TwitterSwiftUI/Core/Feed/FeedView.swift
@@ -0,0 +1,26 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUI
+//
+//  Created by Jose Castillo on 11/4/22.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ... 20, id: \.self) { _ in
+                    Text("Text Row View")
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
