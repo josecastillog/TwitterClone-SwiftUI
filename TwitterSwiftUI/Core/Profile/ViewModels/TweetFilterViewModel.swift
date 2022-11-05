22e6567dea8c6b077eda8f5b6d3704fbfae8303b
josecas <jmcg0901@gmail.com>
2022-11-05 17:27:11 -0600

    Developed ProfileView

---
+++ TwitterSwiftUI/Core/Profile/ViewModels/TweetFilterViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  TweetFilterViewModel.swift
+//  TwitterSwiftUI
+//
+//  Created by Jose Castillo on 11/5/22.
+//
+
+import Foundation
+
+enum TweetFilterViewModel: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var title: String {
+        switch self {
+        case .tweets: return "Tweets"
+        case .replies: return "Replies"
+        case .likes: return "Likes"
+        }
+    }
+}
---
+++ TwitterSwiftUI/Core/Profile/Views/ProfileView.swift
@@ -0,0 +1,175 @@
+//
+//  ProfileView.swift
+//  TwitterSwiftUI
+//
+//  Created by Jose Castillo on 11/5/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var selectedFilter: TweetFilterViewModel = .tweets
+    @Namespace var animation
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            headerView
+            
+            actionButtons
+            
+            userInfoDetails
+            
+            tweetFilterBar
+            
+            tweetsView
+            
+            Spacer()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
+
+extension ProfileView {
+    
+    var headerView: some View {
+        ZStack(alignment: .bottomLeading) {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            VStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20, height: 16)
+                        .foregroundColor(.white)
+                        .offset(x: 4, y: 2)
+                }
+                Circle()
+                    .frame(width: 72, height: 72)
+                .offset(x: 16, y: 24)
+            }
+        }
+        .frame(height: 110)
+    }
+    
+    var actionButtons: some View {
+        HStack(spacing: 12) {
+            Spacer()
+            
+            Image(systemName: "bell.badge")
+                .font(.title3)
+                .padding(6)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 0.75))
+            
+            Button {
+                
+            } label: {
+                Text("Edit Profie")
+                    .font(.subheadline).bold()
+                    .frame(width: 120, height: 32)
+                    .foregroundColor(.black)
+                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.gray, lineWidth: 0.75))
+            }
+
+        }
+        .padding(.trailing)
+    }
+    
+    var userInfoDetails: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Text("Fernando Alonso")
+                    .font(.title2).bold()
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundColor(Color(.systemBlue))
+            }
+            
+            Text("@alo_oficial")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Text("Karting & Formula 1 & Endurance world champion")
+                .font(.subheadline)
+                .padding(.vertical)
+            
+            HStack(spacing: 24) {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Madrid, Spain")
+                }
+                HStack {
+                    Image(systemName: "link")
+                    Text("www.github.com")
+                }
+            }
+            .font(.caption)
+            .foregroundColor(.gray)
+            
+            HStack(spacing: 24) {
+                HStack (spacing: 4) {
+                    Text("134")
+                        .font(.subheadline)
+                        .bold()
+                    Text("Following")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                HStack {
+                    Text("3.2M")
+                        .font(.subheadline)
+                        .bold()
+                    Text("Followers")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding(.vertical)
+        }
+        .padding(.horizontal)
+    }
+    
+    var tweetFilterBar : some View {
+        HStack {
+            ForEach(TweetFilterViewModel.allCases, id: \.rawValue) { item in
+                VStack {
+                    Text(item.title)
+                        .font(.subheadline)
+                        .fontWeight(selectedFilter == item ? .semibold : .regular)
+                        .foregroundColor(selectedFilter == item ? .black : .gray)
+                    
+                    if selectedFilter == item {
+                        Capsule()
+                            .foregroundColor(Color(.systemBlue))
+                            .frame(height: 3)
+                            .matchedGeometryEffect(id: "filter", in: animation)
+                    } else {
+                        Capsule()
+                            .foregroundColor(Color(.clear))
+                            .frame(height: 3)
+                    }
+                }.onTapGesture {
+                    withAnimation(.easeInOut) {
+                        self.selectedFilter = item
+                    }
+                }
+            }
+        }
+        .overlay(Divider().offset(x: 0, y: 16))
+    }
+    
+    var tweetsView: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ... 9, id: \.self) { _ in
+                    TweetRowView()
+                }
+            }
+        }
+    }
+}
