02b1a77071c96bed0a8ce85b04a4a90f62c76b81
josecas <jmcg0901@gmail.com>
2022-11-04 20:03:00 -0600

    Developed TweetRowView

--- TwitterSwiftUI/Core/Components/Tweets/TweetRowView.swift
+++ TwitterSwiftUI/Core/Components/Tweets/TweetRowView.swift
@@ -9,7 +9,69 @@ import SwiftUI
 
 struct TweetRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack(alignment: .top, spacing: 12) {
+                Circle()
+                    .frame(width: 56, height: 56)
+                    .foregroundColor(Color(.systemBlue))
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Bruce Wayne")
+                            .font(.subheadline).bold()
+                        Text("@batman")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                        Text("2w")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                    Text("I am not Batman people of Twitter.")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                }
+            }
+            // Buttons
+            HStack {
+                Button {
+                    // action ...
+                } label: {
+                    Image(systemName: "bubble.left")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    // action ...
+                } label: {
+                    Image(systemName: "arrow.2.squarepath")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    // action ...
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button {
+                    // action ...
+                } label: {
+                    Image(systemName: "bookmark")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            .foregroundColor(.gray)
+            
+            Divider()
+        }
+        .padding()
     }
 }
 
